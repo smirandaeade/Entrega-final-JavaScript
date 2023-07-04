e0a94a23924b12e38fd8e80e3544cce908e7f26d
sebastian miranda <smirandaeade@gmail.com>
2023-07-04 16:56:26 -0400

    Se agrega funcion asincrona y promise en login.js

--- js/login.js
+++ js/login.js
@@ -4,38 +4,27 @@ let userData = null;
 
 const loginForm = document.getElementById('formulario-indexsesion');
 
-loginForm.addEventListener('submit', (event) => {
-
+loginForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-
 
     const email = document.getElementById('correo-index').value;
     const password = document.getElementById('pass-index').value;
 
-    let userFound = false;
+    try {
+        const storedData = await loginUser(email, password);
+        userData = storedData.nombre + ' ' + storedData.apellido1 + ' ' + storedData.apellido2;
+        sessionStorage.setItem('userData', JSON.stringify(userData));
 
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const storedData = JSON.parse(localStorage.getItem(key));
-        if (storedData && storedData.email === email && storedData.password === password) {
-            userFound = true;
-            userData = storedData.nombre + ' ' + storedData.apellido1 + ' ' + storedData.apellido2;
-            sessionStorage.setItem('userData', JSON.stringify(userData));
-            break;
-        }
-    }
-
-    if (userFound) {
-        Swal.fire({
+        await Swal.fire({
             icon: 'success',
             title: 'Inicio de sesión exitoso',
             showConfirmButton: false,
             timer: 1500,
             timerProgressBar: true
-        }).then(() => {
-            window.location.href = 'menu.html';
         });
-    } else {
+
+        window.location.href = 'menu.html';
+    } catch (error) {
         Swal.fire({
             icon: 'error',
             title: 'Correo o contraseña incorrectos',
@@ -44,4 +33,24 @@ loginForm.addEventListener('submit', (event) => {
             timerProgressBar: true
         });
     }
-})
+});
+
+function loginUser(email, password) {
+    return new Promise((resolve, reject) => {
+        let userFound = false;
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const storedData = JSON.parse(localStorage.getItem(key));
+            if (storedData && storedData.email === email && storedData.password === password) {
+                userFound = true;
+                resolve(storedData);
+                break;
+            }
+        }
+
+        if (!userFound) {
+            reject();
+        }
+    });
+}
